9080c0956c20fa07a2e7c935d1c0ca082509883d
RkShaRkz <darksidemetatron@gmail.com>
2024-04-06 19:26:43 +0200

    Median of Two Sorted Arrays - O(log(n)) version
    Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
    
    The overall run time complexity should be O(log (m+n)).
    
    Since we need to hit O(log(n)) I'm guessing we somehow need to use binary something something, like a binary search.
    
    The key difference over O(n) is that we're doing a binary search on just partial subarrays (partitions) - or finding the partition sizes using binary search to be more precise - and then we're calculating the median based off of just half of the elements.
    
    Finding these boundaries looks like a hack, feels like a hack (or a code-smell at least) but is really not.
    
    The real reason why it *still* looks like a hack is due to the fact that the problem statement *NEVER* mentions "sorted and consecutive", which this solution not only implies but also relies on.
    
    EDIT: I stand corrected, it does not rely on it and works fine for non-consecutive arrays. lol.

--- src/MedianOfTwoSortedArrays.java
+++ src/MedianOfTwoSortedArrays.java
@@ -28,7 +28,6 @@ public class MedianOfTwoSortedArrays {
 
             if (!reachedEnd1 && !reachedEnd2) {
                 if (val1 < val2) {
-//                mergedArray[i + j] = nums1[i];
                     mergedArray[i + j] = val1;
 
                     // do sum and median parts
@@ -36,7 +35,6 @@ public class MedianOfTwoSortedArrays {
                     i++;
                     median = (double) totalSum / (i + j);
                 } else {
-//                mergedArray[i + j] = nums2[j];
                     mergedArray[i + j] = val2;
 
                     // do sum and median parts
@@ -77,11 +75,72 @@ public class MedianOfTwoSortedArrays {
         }
     }
 
-    public double findMedianSortedArrays_O_logn(int[] nums1, int[] nums2) {
-        return 0.0f;
+    public static double findMedianSortedArrays_O_logn(int[] nums1, int[] nums2) {
+        /**
+         * To achieve O(log(m+n)) time complexity for finding the median of two sorted arrays, you can use the binary search approach.
+         * Here's a general outline of how you can do it:
+         *
+         * 1. Ensure that the combined length of both arrays (m + n) is even or odd. If it's odd, finding the median is straightforward.
+         * If it's even, you'll need to find the median of two middle elements.
+         *
+         * 2. Perform binary search on the smaller array (let's say nums1) to partition it into two halves such that:
+         * - Elements on the left side are smaller than elements on the right side.
+         * - The total number of elements on the left side is equal to or one less than the total number of elements on the right side.
+         *
+         * 3. Determine the corresponding partition in the other array (nums2) based on the partition in nums1.
+         *
+         * 4. Check if the partitioning is correct:
+         * - If the partitioning is correct, calculate the median based on the values at the partition boundaries.
+         * - If the partitioning is incorrect, adjust the partitioning and continue the binary search.
+         *
+         * 5. Repeat steps 2-4 until the correct partitioning is found.
+         */
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        // Ensure we're using the smaller one as nums1 (i'm not really getting *why* exactly we want the first array to be smaller though)
+        if (m > n) {
+            return findMedianSortedArrays_O_logn(nums2, nums1);
+        }
+
+        int totalLength = m + n;
+        // Initializing binary search boundaries
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+            int partitionNums1 = (low + high) / 2;
+            int partitionNums2 = (totalLength + 1) / 2 - partitionNums1;
+
+            int maxLeftNums1 = (partitionNums1 != 0) ? nums1[partitionNums1 - 1] : Integer.MIN_VALUE;
+            int minRightNums1 = (partitionNums1 != m) ? nums1[partitionNums1] : Integer.MAX_VALUE;
+
+            int maxLeftNums2 = (partitionNums2 != 0) ? nums2[partitionNums2 - 1] : Integer.MIN_VALUE;
+            int minRightNums2 = (partitionNums2 != n) ? nums2[partitionNums2] : Integer.MAX_VALUE;
+
+            if (maxLeftNums1 <= minRightNums2 && maxLeftNums2 <= minRightNums1) {
+                if (totalLength % 2 == 0) {
+                    return (Math.max(maxLeftNums1, maxLeftNums2) + Math.min(minRightNums1, minRightNums2)) / 2.0;
+                } else {
+                    return Math.max(maxLeftNums1, maxLeftNums2);
+                }
+            } else if (maxLeftNums1 > minRightNums2) {
+                high = partitionNums1 - 1; // Partition is too far to the right, adjust high
+            } else {
+                low = partitionNums1 + 1; // Partition is too far to the left, adjust low
+            }
+        }
+
+        // Should never reach here
+        return -1;
     }
 
     public static void main(String[] args) {
+        /**
+         * Original O(n) version
+         */
+        System.out.println(">>>>>>>>>> O(n) version <<<<<<<<<<");
         int[] nums1 = {1, 3};
         int[] nums2 = {2};
         double expected = 2.0f;
@@ -90,11 +149,40 @@ public class MedianOfTwoSortedArrays {
 
         stringifyCase(nums1, nums2, expected, actualOutput);
 
-        nums1 = new int[] {1,2};
-        nums2 = new int[] {3,4};
+        nums1 = new int[]{1, 2};
+        nums2 = new int[]{3, 4};
         expected = 2.5f;
 
         actualOutput = findMedianSortedArrays_O_n(nums1, nums2);
+
+        stringifyCase(nums1, nums2, expected, actualOutput);
+
+        /**
+         * The desired O(log(n)) version
+         */
+        System.out.println(">>>>>>>>>> O(log(n)) version <<<<<<<<<<");
+        nums1 = new int[]{1, 3};
+        nums2 = new int[]{2};
+        expected = 2.0f;
+
+        actualOutput = findMedianSortedArrays_O_logn(nums1, nums2);
+
+        stringifyCase(nums1, nums2, expected, actualOutput);
+
+        nums1 = new int[]{1, 2};
+        nums2 = new int[]{3, 4};
+        expected = 2.5f;
+
+        actualOutput = findMedianSortedArrays_O_logn(nums1, nums2);
+
+        stringifyCase(nums1, nums2, expected, actualOutput);
+
+        // My case - test if it works for nonconsecutive arrays
+        nums1 = new int[]{1, 3, 5, 7};
+        nums2 = new int[]{2, 6, 8, 12};
+        expected = 5.5f;
+
+        actualOutput = findMedianSortedArrays_O_logn(nums1, nums2);
 
         stringifyCase(nums1, nums2, expected, actualOutput);
     }
